50860c3ba6126f3c4da9de45e3cef1e149fd788f
Rafal Piotrowski <rafalpiotrowski@users.noreply.github.com>
2021-09-19 20:03:52 +0200

    valid parentheses solved

--- src/valid_parentheses.rs
+++ src/valid_parentheses.rs
@@ -1,9 +1,37 @@
+/*
+Runtime: 0 ms, faster than 100.00% of Rust online submissions for Valid Parentheses.
+Memory Usage: 1.9 MB, less than 100.00% of Rust online submissions for Valid Parentheses.
+*/
 pub fn is_valid(s: String) -> bool {
-    if s.len() == 0 || s.len() % 2 > 0 {
+
+    if s.len() == 0 || (s.len()%2) != 0 {
         return false;
     }
-    //for let c in s[0..(s.len()/2)] {
-        
-    //}
-    true
+
+    let mut stack = vec!();
+    let chars = s.chars().collect::<Vec<char>>();
+
+    for c in chars.iter() {
+        match c {
+            '{' | '[' | '(' => {
+                //println!("pushing {}", c);
+                stack.push(c)
+            },
+            _ => {
+                match stack.pop() {
+                    None => return false,
+                    Some(x) => {
+                        //println!("pop => c = {}, x = {}", c, x);
+                        match (x, c) {
+                            ('{', '}') => continue,
+                            ('[', ']') => continue,
+                            ('(', ')') => continue,
+                            (_, _) => return false
+                        }
+                    }
+                }
+            }
+        }
+    }
+    stack.len() == 0
 }
--- tests/valid_parentheses.rs
+++ tests/valid_parentheses.rs
@@ -10,6 +10,31 @@ mod tests {
 
     #[test]
     fn test_2() {
-        assert_eq!(is_valid("[[[".to_string()), false)
+        assert_eq!(is_valid("()[]{}".to_string()), true)
+    }
+
+    #[test]
+    fn test_3() {
+        assert_eq!(is_valid("(]".to_string()), false)
+    }
+
+    #[test]
+    fn test_4() {
+        assert_eq!(is_valid("([)]".to_string()), false)
+    }
+
+    #[test]
+    fn test_5() {
+        assert_eq!(is_valid("{[]}".to_string()), true)
+    }
+
+    #[test]
+    fn test_6() {
+        assert_eq!(is_valid("(".to_string()), false)
+    }
+
+    #[test]
+    fn test_7() {
+        assert_eq!(is_valid("((".to_string()), false)
     }
 }
